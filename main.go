dee1770b368d116afaa1f5e18de144ed7960a839
CarlosBueloni <carlosbueloni@gmail.com>
2025-03-10 11:35:57 -0300

    add api namespace

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ func main() {
 	mux := http.NewServeMux()
 	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET  /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET  /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
