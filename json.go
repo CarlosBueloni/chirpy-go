02b4570ce5028fb129a9e3adb4b0cff1b832f94f
CarlosBueloni <carlosbueloni@gmail.com>
2025-03-11 15:55:10 -0300

    add profanity filter

--- json.go
+++ json.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"io"
 	"net/http"
 )
 
@@ -19,33 +18,4 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error
 
 func respondWithError(w http.ResponseWriter, code int, msg string) error {
 	return respondWithJSON(w, code, map[string]string{"error": msg})
-}
-
-func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-
-	type requestBody struct {
-		Body string `json:body`
-	}
-
-	dat, err := io.ReadAll(r.Body)
-	if err != nil {
-		respondWithError(w, 500, "couldn't read request")
-		return
-	}
-
-	params := requestBody{}
-	err = json.Unmarshal(dat, &params)
-	if err != nil {
-		respondWithError(w, 500, "couldn't unmarshal parameters")
-		return
-	}
-
-	if len(params.Body) > 140 {
-		respondWithError(w, 400, "Chirp is to long")
-		return
-	}
-
-	respondWithJSON(w, 200, map[string]bool{"valid": true})
-
 }
