2c7d94d70cd9f921f0f969869b6d52fbe1299621
CarlosBueloni <carlosbueloni@gmail.com>
2025-03-13 22:23:36 -0300

    add basic password hashing

---
+++ internal/auth/hash.go
@@ -0,0 +1,18 @@
+package auth
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hashed_password, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", err
+	}
+	return string(hashed_password), nil
+}
+
+func CheckPasswordHash(password, hash string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
+		return err
+	}
+	return nil
+}
