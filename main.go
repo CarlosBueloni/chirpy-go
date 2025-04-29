6eaa4a6def1dac5ded3e4c2d99dcda25b74f1d8d
CarlosBueloni <carlosbueloni@gmail.com>
2025-04-29 17:15:11 -0300

    refactor

--- main.go
+++ main.go
@@ -13,13 +13,29 @@ type apiConfig struct {
 func main() {
 	const port = "8080"
 	const filepathRoot = "/app/"
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle(filepathRoot, http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle(filepathRoot, apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET  /api/healthz", handlerReadiness)
+
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET  /admin/metrics", apiCfg.handlerMetrics)
+
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
